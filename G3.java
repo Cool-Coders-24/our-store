7802a16eb727fcccd105737aaa07dd1c55f4815d
Cool-Coders-24 <coolcoders24@gmail.com>
2025-09-17 19:26:06 +0530

    Update G3.java

--- G3.java
+++ G3.java
@@ -1,18 +1,1 @@
-class G1
-{
-	int id = 100;
-	String name = "guddu";
-}
-class G2 extends g1
-{
-	String cName = "Rec";
-}
-class  G3 extends G1,G2
-     double sallary = 10000.00;
-{
-	public static void main(String[] args) 
-	{
-		g3 g = new G3();
-	
-	}
-}
+i phhone 16=10;000
